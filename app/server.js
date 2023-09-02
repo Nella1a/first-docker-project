4182821c4e07547ee66c0ea925d908fc4ffe52cd
Nella1a <m.allen@gmx.at>
2023-09-02 20:32:28 +0200

    Add port for the server to know to listen for incoming requests

--- app/server.js
+++ app/server.js
@@ -10,3 +10,7 @@ const port = 5000;
 app.get('/', function (req, res) {
   res.send('GET request to homepage');
 });
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
