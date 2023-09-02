96477456010cf7b6de0d3404f5666314f191f972
Nella1a <m.allen@gmx.at>
2023-09-02 20:26:06 +0200

    Add port variable

--- app/server.js
+++ app/server.js
@@ -1,5 +1,8 @@
 // import express
 const express = require('express');
 
-//create an instance of the app object in express
+// create an instance of the app object in express
 const app = express();
+
+// add port variable
+const port = 5000;
